737db2985e7153b38caf105496898821d6ff346e
guoyongwei <guoyongwei@gomefinance.com.cn>
2019-02-12 18:23:31 +0800

    本地关联远程仓库 提交代码

--- src/router.js
+++ src/router.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import HelloWorld from '@/views/HelloWorld'
+import HomePage from '@/views/HomePage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/homepage',
+      name: 'homepage',
+      component: HomePage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/',
+      name: 'helloword',
+      component: HelloWorld
     }
   ]
 })
